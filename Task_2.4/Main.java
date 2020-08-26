e67c7b99a843ed120eebb7c20256c9939b0621f7
SnegniyLev <70105682+SnegniyLev@users.noreply.github.com>
2020-08-26 14:18:11 +0200

    Update Main.java

--- Task_2.4/Main.java
+++ Task_2.4/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        System.out.println(a);
-        System.out.println(a+b);
-        System.out.println(a+b+c);
-        System.out.println(a+b+c+d);
-        System.out.println(a+b+c+d+e);
+        int result = 0;
+        for(int i=1; i<=5; i++)
+            result+=i;
+        System.out.println(result);
     }
 }
