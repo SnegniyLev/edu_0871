a2bf114c2f5acf9e09c848ef58a91c60b7c33c95
SnegniyLev <70105682+SnegniyLev@users.noreply.github.com>
2020-08-26 14:27:15 +0200

    Create Dog.java

---
+++ Task_3.3/Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+    private String name;
+    private int age;
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args) {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
